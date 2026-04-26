aaa1f63dac738c8eebf8da0d079da95b58dd8bd0
ILuv-2Code <davecollege4473+github@gmail.com>
2026-04-26 11:30:34 -0400

    renamed demo file

---
+++ Milestone_3/demo_milestone3.py
@@ -0,0 +1,8 @@
+# Demo Script Developed by David Matos
+# Runs all demos for milestone 1-3
+
+# demo 1
+
+# demo 2
+
+# demo 3
--- Milestone_3/demo_milestone_all.py
+++
@@ -1,2 +0,0 @@
-# Demo Script Developed by David Matos
-# Runs all demos for milestone 1-3
